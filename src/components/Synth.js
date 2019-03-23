3f14ec52f8db3bf65cbc393fb08774fe6aac4790
mscbr <scibor.maciej@gmail.com>
2019-03-23 23:41:19 +0100

    initial dataflow for sequencer designed

--- src/components/Synth.js
+++ src/components/Synth.js
@@ -9,22 +9,101 @@ class Synth extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            
+          partch: [1200.0,
+            1178.49371,
+            1146.727057,
+            1115.532807,
+            1088.268715,
+            1049.362941,
+            1034.995772,
+            1017.596288,
+            996.0899983,
+            968.8259065,
+            933.1290944,
+            905.8650026,
+            884.358713,
+            852.5920594,
+            813.6862861,
+            782.4920359,
+            764.9159047,
+            729.2190927,
+            701.9550009,
+            680.4487113,
+            648.6820576,
+            617.4878074,
+            582.5121926,
+            551.3179424,
+            519.5512887,
+            498.0449991,
+            470.7809073,
+            435.0840953,
+            417.5079641,
+            386.3137139,
+            347.4079406,
+            315.641287,
+            294.1349974,
+            266.8709056,
+            231.1740935,
+            203.9100017,
+            182.4037121,
+            165.0042285,
+            150.6370585,
+            111.7312853,
+            84.46719347,
+            53.27294323,
+            21.5062896,
+            0.0],
+          sequenceValues: [440,445,220,220],
+          seqencePosition: 0
         }
+
+        ///INITIAL SET-UP FOR SYNTH & SEQUENCER
+        this.synth = new Tone.Synth().toMaster();
+        this.seq = new Tone.Sequence((time, value) => {
+          console.log(Tone.Transport.position.slice(2,5));
+          this.positionSet();
+          
+          this.synth.triggerAttackRelease(value, "16n", time);
+        }, this.state.sequenceValues, "16n");
+       
         
     }
     componentDidMount() {
-      this.synth = new Tone.Synth();
-      this.synth.toMaster();
-      console.log(this.synth);
-      this.seq = new Tone.Sequence((time, value) => {
-        console.log(this.synth);
-        this.synth.triggerAttackRelease(value, "16n", time);
-      }, ["C4", "E2"], "16n");
-      //this.seq.loop = true;
-      
+      //INITIALLY SETTING UP RANDOMIZED SEQ VALUES
+      this.setState({
+        sequenceValues: this.randomizeSequence()
+      });
+    }
+    componentDidUpdate(prevProps, prevState) {
+      //updating sequence values
+      if (prevState.sequenceValues !== this.state.sequenceValues) {
+        this.seq = new Tone.Sequence((time, value) => {
+          this.positionSet();
+          this.synth.triggerAttackRelease(value, "16n", time);
+        }, this.state.sequenceValues, "16n");
+      }
+    }
+    positionSet = () => {let seqPosVal = (parseInt(Tone.Transport.position.slice(0,1))%2)*16 + parseInt(Tone.Transport.position.slice(2,3))*4 + parseInt(Tone.Transport.position.slice(4,5));
+      this.setState({
+        seqencePosition: seqPosVal
+      })
+    }
+    randomizeSequence = () => {
+      let sequence = [];
+      const { partch } = this.state;
+      for (let i = 0; i < 32; i++) {
+        sequence.push(i%4 ? 110 : i === 0 ?  2400 : partch[Math.floor(Math.random() * i)]);
+      }
+      return sequence;
     }
     triggerSeq = () => {
+      //###########################################################################
+      //###with code below you can create (w/ chrome) chip-sound effect on synth###
+      //###########################################################################
+      // this.setState({
+      //   sequenceValues: this.randomizeSequence()
+      // })
+    
       this.seq.start();
       Tone.Transport.start();
     }
@@ -33,6 +112,7 @@ class Synth extends Component {
       this.seq.stop();
     }
   render() {
+    
     return (
       <div className="synth">
         <div className="oscillator">
@@ -40,8 +120,8 @@ class Synth extends Component {
         </div> 
         <div className="control">
           <div className="seq">
-          SEQ
-          <Sequencer triggerSeq={this.triggerSeq} stopSeq={this.stopSeq} />
+          SEQ  {Tone.Transport.bpm.value}
+          <Sequencer triggerSeq={this.triggerSeq} stopSeq={this.stopSeq} seqPosition={this.state.seqencePosition} />
           </div>
           <div className="adsr">
           ADSR
--- src/components/seq/Sequencer.js
+++ src/components/seq/Sequencer.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 const rowStyle = {
   display: 'flex',
@@ -7,25 +8,33 @@ const rowStyle = {
 }
 const stepStyle = {
   width: '5%',
-  height: '3vh',
+  height: '8vh',
   border: '1px solid red',
+  margin: 2,
+  borderRadius: 3
 }
-
+const progressStyle = {
+  width: '100%',
+  height: '15%',
+  border: '1px solid black'
+}
+//CHANGE THIS TO FUNCTIONAL COMPONENT
 export default class Sequencer extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            triggerSeq: props.triggerSeq,
-            stopSeq: props.stopSeq
+            test: 0
         }
     }
 
     makeRow = () => {
       const steps = [];
       
-      for(let i = 0; i < 33; i++) {
+      for(let i = 0; i < 32; i++) {
         steps.push(
-          <div style={stepStyle}>
+          
+          <div style={stepStyle} key={`step${i}`}>
+          <div style={progressStyle} index={`styleIndex${i}`}></div>
           {i}
           </div>
         );
@@ -35,18 +44,30 @@ export default class Sequencer extends Component {
     }
 
   render() {
+    console.log(this.props.seqPosition);
     const steps = this.makeRow();
-    console.log(steps);
+    //console.log(this.state.);
     return (
       <div className='s'>
-        <button onClick={this.state.triggerSeq}>>>></button>
-        <button onClick={this.state.stopSeq}>|||</button>
+        <button onClick={this.props.triggerSeq}>>>></button>
+        <button onClick={this.props.stopSeq}>|||</button>
         <div style={rowStyle}>
           {steps.map((step, index) => {
             return index<16 ? step : null;
           })}
         </div>
+        <div style={rowStyle}>
+          {steps.map((step, index) => {
+            return index>15 ? step : null;
+          })}
+        </div>
       </div>
     )
   }
+}
+
+Sequencer.propTypes = {
+  triggerSeq: PropTypes.func,
+  stopSeq: PropTypes.func,
+  seqPosition: PropTypes.number
 }
