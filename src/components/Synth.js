341b277e5d85c30db57d0531980bf00b380aaec0
mscbr <scibor.maciej@gmail.com>
2019-03-28 00:56:20 +0100

    amp A&R sliders fully develped

--- src/components/Synth.js
+++ src/components/Synth.js
@@ -15,7 +15,7 @@ class Synth extends Component {
         adsr: {
           ampAttack: 0.01,
           ampRelease: 0.4,
-          filtAttack: 0.01,
+          filtSustain: 1,
           filtDecay: 0, 
         },
         sequenceValues: [''],
@@ -26,32 +26,16 @@ class Synth extends Component {
       }
 
       ///INITIAL SET-UP FOR SYNTH & SEQUENCER
-      this.synth = new Tone.DuoSynth(
-        {
-          "voice0": {
-            "envelope": {
-              "attack": this.state.adsr.ampAttack,
-              "release": this.state.adsr.ampRelease
-            },
-            "filterEnvelope": {
-              "attack": this.state.adsr.filtAttack,
-              "decay": this.state.adsr.filtDecay
-            },
-            "oscillator": {
-              "type": "triangle"
-            },
-            //"filterEmve"
-          }
-        }
-      );
-      console.log('state');
-      console.log(this.state);
-      // this.synth.voice0.envelope.attack = 0.4;
-      // this.synth.voice0.envelope.release = 0.8;
-      // this.synth.voice1.envelope.attack = 0.4;
+      this.synth = new Tone.DuoSynth();
       this.gain = new Tone.Gain(0.7).toMaster();
       
-      this.synth.connect(this.gain);
+      //!!!!!!!!!!
+      //MODULATE THIS INSTEAD OF filterEnvelope of synth
+      this.lowPass = new Tone.Filter(250, 'lowpass', (-24));
+      
+      
+      
+      this.synth.connect(this.lowPass.connect(this.gain));
       this.seq = new Tone.Sequence((time, value) => {
         this.positionSet();
         if(value !== "") {
@@ -87,8 +71,20 @@ class Synth extends Component {
     }
     //ADSR values update
     if (prevState.adsr !== this.state.adsr) {
+      //AMP ATTACK
       this.synth.voice0.envelope.attack = this.state.adsr.ampAttack;
       this.synth.voice1.envelope.attack = this.state.adsr.ampAttack;
+      //AMP RELEASE
+      
+      this.synth.voice0.envelope.release = this.state.adsr.ampRelease;
+      this.synth.voice1.envelope.release = this.state.adsr.ampRelease;
+      //FILTER DECAY
+      this.synth.voice0.filterEnvelope.decay = this.state.adsr.filtDecay;
+      this.synth.voice1.filterEnvelope.decay = this.state.adsr.filtDecay;
+      //FILTER SUSTAIN
+      this.synth.voice0.filterEnvelope.sustain = this.state.adsr.filtSustain;
+      this.synth.voice1.filterEnvelope.sustain = this.state.adsr.filtSustain;
+      //console.log(this.synth.voice0.filterEnvelope.decay);
     }
   }
 
@@ -148,7 +144,8 @@ class Synth extends Component {
   handleAdsr = (e) => {
     this.setState({
       adsr: {
-        [e.target.id]: e.target.value
+        ...this.state.adsr,
+        [e.target.id]: parseFloat(e.target.value)
       }
     });
   }
@@ -181,6 +178,7 @@ class Synth extends Component {
   //build a conditional render only for step sequencer
   //IF ONLY POS CHANGED ONLY RENDER ~~
   render() {
+    
     const button = this.handlePlayButton();
     return (
       <div className="synth">
--- src/components/adsr/Adsr.js
+++ src/components/adsr/Adsr.js
@@ -2,20 +2,54 @@ import React from 'react'
 
 
 const Adsr = (props) => {
-    console.log(props);
-    const handleAdsr = (e) => {
-        console.log(e.target.id);
-    }
+    
     return (
         <div className='adsr-sliders'>
-            <div className="slider-wrapper">
-                <input id="ampAttack" type="range" 
-                    min={0.1} max={4} 
-                    value={props.adsrVal.ampAttack} step={0.1} 
-                    onChange={props.handleAdsr}
-                />
+            
+            <div className="adsr-slider">
+                <span>AMP</span>
+                <div className="slider-wrapper">
+                    <input id="ampAttack" type="range" 
+                        min={0.1} max={4} 
+                        value={props.adsrVal.ampAttack} step={0.1} 
+                        onChange={props.handleAdsr}
+                    />
+                </div>
+                <span>A: <br />{props.adsrVal.ampAttack}</span>
             </div>
-            <span>VAL: {props.adsrVal.ampAttack}</span>
+            <div className="adsr-slider">
+                <span>AMP</span>
+                <div className="slider-wrapper">
+                    <input id="ampRelease" type="range" 
+                        min={0.1} max={3} 
+                        value={props.adsrVal.ampRelease} step={0.1} 
+                        onChange={props.handleAdsr}
+                    />
+                </div>
+                <span>R: <br />{props.adsrVal.ampRelease}</span>
+            </div>
+            <div className="adsr-slider">
+                <span>FILT</span>
+                <div className="slider-wrapper">
+                    <input id="filtSustain" type="range" 
+                        min={0.1} max={1} 
+                        value={props.adsrVal.filtSustain} step={0.1} 
+                        onChange={props.handleAdsr}
+                    />
+                </div>
+                <span>S: <br />{props.adsrVal.filtSustain}</span>
+            </div>
+            <div className="adsr-slider">
+                <span>FILT</span>
+                <div className="slider-wrapper">
+                    <input id="filtDecay" type="range" 
+                        min={0.1} max={2} 
+                        value={props.adsrVal.filtDecay} step={0.1} 
+                        onChange={props.handleAdsr}
+                    />
+                </div>
+                <span>D: <br />{props.adsrVal.filtDecay}</span>
+            </div>
         </div>
     );
 }
