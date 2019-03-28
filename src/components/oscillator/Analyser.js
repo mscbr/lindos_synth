6c75368a1bd16ab31a837b2db6649b8f8f586c33
mscbr <scibor.maciej@gmail.com>
2019-03-28 21:38:25 +0100

    code cleanup

--- src/components/oscillator/Analyser.js
+++ src/components/oscillator/Analyser.js
@@ -5,16 +5,9 @@ import Partch from '../../partch301014b.png'
 class Analyser extends Component {
     constructor(props) {
         super(props);
-        
         this.canvasRef = React.createRef();
     }
-
-    componentDidMount() {
-        //this.updateCanvas();
-        
-    }
     componentDidUpdate() {
-        //this.updateCanvas();
         const dataArray = this.props.waveArr;
         const canvas = this.canvasRef.current;
         const ctx = canvas.getContext('2d');
@@ -32,42 +25,12 @@ class Analyser extends Component {
             if(i===0) {
                 ctx.moveTo(x, y);
             } else {
-                //console.log(x, y);
-                ctx.lineTo(x, y);
-            }
-            x += sliceWidth;
-            //ctx.stroke();
-        }
-        ctx.stroke();
-        
-    }
-    updateCanvas() {
-        let ctx = this.refs.canvas.getContext('2d');
-        
-        ctx.strokeStyle = 'rgba(255, 252, 246, 1)';
-        ctx.lineWidth = 3;
-        ctx.beginPath();
-        let sliceWidth = 512*1.0/this.dataArray.length;
-        let x=0;
-        for(let i = 0; i<this.dataArray.length; i++) {
-            let v = this.dataArray[i] / 128.0;
-            let y = v*140/2;
-            if(i===0) {
-                ctx.moveTo(x, y);
-            } else {
-                //console.log(x, y);
                 ctx.lineTo(x, y);
             }
             x += sliceWidth;
         }
-        
-        // ctx.moveTo(0, 70);
-        // ctx.lineTo(100, 45);
-        // ctx.lineTo(256, 75);
-
-        // ctx.lineTo(512, 60);
         ctx.stroke();
-        //this.setState();
+        
     }
 
     render() {
