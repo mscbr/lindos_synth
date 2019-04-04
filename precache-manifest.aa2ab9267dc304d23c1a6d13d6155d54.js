c1f97078d2ce83636aaff10876d630549ecd80b5
mscbr <scibor.maciej@gmail.com>
2019-04-04 12:18:11 +0200

    Updates

---
+++ precache-manifest.aa2ab9267dc304d23c1a6d13d6155d54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0fb2427b7bc6149aecbf189973574c1a",
+    "url": "/static/media/lindos-synth-logo.0fb2427b.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5a1db45ac6db2f7eab05",
+    "url": "/static/js/main.11f94940.chunk.js"
+  },
+  {
+    "revision": "271f73d9dce604819694",
+    "url": "/static/js/2.4d6aacd7.chunk.js"
+  },
+  {
+    "revision": "5a1db45ac6db2f7eab05",
+    "url": "/static/css/main.d90d1ef9.chunk.css"
+  },
+  {
+    "revision": "288f22815c6d9730acbd3dabf365e471",
+    "url": "/index.html"
+  }
+];
